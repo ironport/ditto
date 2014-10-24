e37cb99ac84b72fc42f1ddb8179bfd7b4e0313a3
Kyle Derr <kyle.derr@civitaslearning.com>
2014-10-24 15:08:00 -0500

    Rev version.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='ditto',
-      version='1.2.3',
+      version='1.2.4',
       description='Python Class Mocking Framework',
       author='Kyle Derr',
       author_email='kyle.derr@gmail.com',
